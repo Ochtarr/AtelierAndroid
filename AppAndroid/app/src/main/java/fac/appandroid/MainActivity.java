062c55260aa7f653283bb5921bf82626e393c611
Florian .C <cathelineauflorian@gmail.com>
2017-10-25 11:14:15 +0200

    add lifecycle methods on main activity

--- AppAndroid/app/src/main/java/fac/appandroid/MainActivity.java
+++ AppAndroid/app/src/main/java/fac/appandroid/MainActivity.java
@@ -23,6 +23,38 @@ public class MainActivity extends AppCompatActivity {
         onClickBtServer();
     }
 
+    @Override
+    protected void onStart(){
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+    }
+
     private void onClickBtClient() {
         btClient.setOnClickListener(new View.OnClickListener(){
             @Override
