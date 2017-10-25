7ce059fde5ea44dd7b6523ba23450bb057cc4341
Florian .C <cathelineauflorian@gmail.com>
2017-10-25 11:26:13 +0200

    add lifecycle methods on server activity + handles log for server and main activities

--- AppAndroid/app/src/main/java/fac/appandroid/MainActivity.java
+++ AppAndroid/app/src/main/java/fac/appandroid/MainActivity.java
@@ -3,16 +3,20 @@ package fac.appandroid;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     private Button btServer;
     private Button btClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -25,33 +29,39 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onStart(){
+        Log.d(TAG, "onStart");
         super.onStart();
     }
 
     @Override
     protected void onRestart(){
+        Log.d(TAG, "onRestart");
         super.onRestart();
     }
 
     @Override
     protected void onResume()
     {
+        Log.d(TAG, "onResume");
         super.onResume();
     }
 
     @Override
     protected void onPause()
     {
+        Log.d(TAG, "onPause");
         super.onPause();
     }
 
     @Override
     protected void onStop(){
+        Log.d(TAG, "onStop");
         super.onStop();
     }
 
     @Override
     protected void onDestroy(){
+        Log.d(TAG, "onDestroy");
         super.onDestroy();
     }
 
