da7a90f999d032cac105ec5fbfe13f6a9218f7bc
Florian .C <cathelineauflorian@gmail.com>
2017-10-27 10:15:36 +0200

    update player Salope !

--- AppAndroid/app/src/main/java/fac/appandroid/Player.java
+++ AppAndroid/app/src/main/java/fac/appandroid/Player.java
@@ -1,13 +1,38 @@
 package fac.appandroid;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import static java.nio.file.Paths.get;
 
 public class Player extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String receivedFilePathName = savedInstanceState.get("receivedFilePathName").toString();
         setContentView(R.layout.activity_player);
+
+        // Get a reference to the VideoView instance as follows, using the id we set in the XML layout.
+        VideoView vidView = (VideoView)findViewById(R.id.video);
+
+        // Add playback controls.
+        MediaController vidControl = new MediaController(this);
+        // Set it to use the VideoView instance as its anchor.
+        vidControl.setAnchorView(vidView);
+        // Set it as the media controller for the VideoView object.
+        vidView.setMediaController(vidControl);
+
+        // Prepare the URI for the endpoint.
+        String vidAddress = "android.resource://" + getPackageName() + "/" + R.raw.video;
+        Uri vidUri = Uri.parse(vidAddress);
+        // Parse the address string as a URI so that we can pass it to the VideoView object.
+        vidView.setVideoURI(vidUri);
+        // Start playback.
+        vidView.start();
+
     }
 }
