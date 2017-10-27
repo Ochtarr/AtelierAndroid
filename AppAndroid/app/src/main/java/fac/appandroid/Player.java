2e4d9e6cb3ea0211613e1626ca858d9fea370405
Florian .C <cathelineauflorian@gmail.com>
2017-10-27 13:33:24 +0200

    Simplify ntm

--- AppAndroid/app/src/main/java/fac/appandroid/Player.java
+++ AppAndroid/app/src/main/java/fac/appandroid/Player.java
@@ -1,16 +1,28 @@
 package fac.appandroid;
 
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.MediaController;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 import java.io.File;
+import java.util.UUID;
 
 import static java.nio.file.Paths.get;
 
 public class Player extends AppCompatActivity {
+
+    public final UUID MY_UUID = UUID.randomUUID();
+
+    //for log
+    private static final String TAG = "PlayerActivity";
+
+    protected VideoView vidView;
+    private boolean hasBeenPlayed; // boolean pour savoir si on a quitté l'app en cours de lecture, utilisé pour reprendre la vidéo
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +31,7 @@ public class Player extends AppCompatActivity {
         setContentView(R.layout.activity_player);
 
         // Get a reference to the VideoView instance as follows, using the id we set in the XML layout.
-        VideoView vidView = (VideoView)findViewById(R.id.video);
+        vidView = (VideoView)findViewById(R.id.video);
 
         // Add playback controls.
         MediaController vidControl = new MediaController(this);
@@ -31,8 +43,36 @@ public class Player extends AppCompatActivity {
         Uri vidUri = Uri.fromFile(new File(receivedFilePathName));
         // Parse the address string as a URI so that we can pass it to the VideoView object.
         vidView.setVideoURI(vidUri);
+
+        // just before the video starts, we display on log the duration of the video
+        //vidView.setOnPreparedListener(new
+        //    MediaPlayer.OnPreparedListener()  {
+        //        @Override
+        //        public void onPrepared(MediaPlayer mp) {
+        //            Log.i(TAG, "Duration = " +
+        //                    vidView.getDuration());
+        //            Toast.makeText(getApplicationContext(), vidView.getDuration(), Toast.LENGTH_SHORT).show();
+        //        }
+        //    }
+        //);
         // Start playback.
         vidView.start();
 
+    }
+
+    protected void onPause(){
+        super.onPause();
+        if(vidView.isPlaying()){
+            hasBeenPlayed = true;
+            vidView.pause();
+        }
+    }
+
+    protected void onResume(){
+        super.onResume();
+        if(hasBeenPlayed == true){
+            hasBeenPlayed = false;
+            vidView.resume();
+        }
     }
 }
